46d5da508aaf5280fce1ec8b9875a6452ff95b98
toby <tobyg7794@hotmail.com>
2020-09-19 01:08:53 +0100

    2d coordinates class added

---
+++ src/main/java/toby/challenge/coordinate/TwoDimensionCoordinates.java
@@ -0,0 +1,30 @@
+package toby.challenge.coordinate;
+
+public class TwoDimensionCoordinates {
+    private int xCoordinate;
+    private int yCoordinate;
+
+    public TwoDimensionCoordinates() {
+    }
+
+    public TwoDimensionCoordinates(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+}
